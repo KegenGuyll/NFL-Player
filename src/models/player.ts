38f9add01b26b273cc679e530735aa5faa3ab20d
Kegen Guyll <guyllkegen@gmail.com>
2021-08-08 21:11:13 -0500

    created player model

---
+++ src/models/player.ts
@@ -0,0 +1,11 @@
+export interface Player {
+  _id: string;
+  name: string;
+  link: string;
+  position: string;
+  height: string;
+  weight: string;
+  born: string;
+  profilePicture: string;
+  teams: [string];
+}
